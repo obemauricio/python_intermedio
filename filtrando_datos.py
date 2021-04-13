93a4353e00d24880d3bca6d5d4d0e0018ed527ef
Obe Mauricio <mauricioobey@gmail.com>
2021-04-13 17:08:00 -0400

    filtrado de datos

---
+++ filtrando_datos.py
@@ -0,0 +1,86 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+def run():
+    all_python_devs = [worker["name"] for worker in DATA if worker["language" ] == "python"]
+    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA)) #retorna todo el dicc mayores a 18
+    adults = list(map(lambda worker: worker["name"], adults)) #mapea a adults y sobreescribe.
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+        #old_pleople tiene una nueva funcion | que añade una nueva categoria al diccionario.
+    for worker in old_people:
+        print(worker)
+
+
+if __name__ == '__main__':
+    run()
