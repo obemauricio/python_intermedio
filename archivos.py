ed7c98b88c2439172a647550c306f872eacdb857
Obe Mauricio <mauricioobey@gmail.com>
2021-04-14 15:01:14 -0400

    archivos

--- archivos.py
+++ archivos.py
@@ -1,5 +1,21 @@
+def read():
+    numbers = []
+    with open("./archive/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    
+    print(numbers)
+
+def write():
+    name = ["facundo", "Miguel", "Pepe", "Christian"]
+    with open("./archive/names.txt", "w", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.wirte("\n")
+
+
 def run():
-    pass
+    read()
 
 
 if __name__ == '__main__':
